7d4eb6b7eb2f9bb20412079c22b3319952109635
water76016 <dxylovelq@163.com>
2023-09-24 16:15:37 +0800

    [add]:添加删除有序数组中的重复项2的解答

--- daixiaoyu-leetcode/src/main/java/com/daixiaoyu/daixiaoyuleetcode/array/删除有序数组中的重复项2.java
+++ daixiaoyu-leetcode/src/main/java/com/daixiaoyu/daixiaoyuleetcode/array/删除有序数组中的重复项2.java
@@ -1,13 +1,32 @@
-package com.daixiaoyu.daixiaoyuleetcode.array;/**
-* @program: advance
-*
-* @author: water76016
-*
-* @description: 
-*
-* @version: v1.0.0
-*
-* @create: 2023-09-14 00:45
-**/
+package com.daixiaoyu.daixiaoyuleetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @program: advance
+ * @author: water76016
+ * @description:
+ * @version: v1.0.0
+ * @create: 2023-09-14 00:45
+ **/
 public class 删除有序数组中的重复项2 {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null){
+            return 0;
+        }
+        if (nums.length <= 2){
+            return nums.length;
+        }
+        int pre = 0;
+        for (int i = 1; i < nums.length; i++){
+            int val = nums[i];
+            //当前面的数和后面的数不相等的时候,或者隔开一个数也不相等的时候，说明可以移动，因为是有序的
+            if (nums[pre] != val || (pre - 1 < 0) || (pre - 1 >= 0 && nums[pre - 1] != val)){
+                pre++;
+                nums[pre] = val;
+            }
+        }
+        return pre + 1;
+    }
 }
--- daixiaoyu-leetcode/src/test/java/com/daixiaoyu/daixiaoyuleetcode/array/删除有序数组中的重复项2Test.java
+++ daixiaoyu-leetcode/src/test/java/com/daixiaoyu/daixiaoyuleetcode/array/删除有序数组中的重复项2Test.java
@@ -1,4 +1,24 @@
+package com.daixiaoyu.daixiaoyuleetcode.array;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class 删除有序数组中的重复项2Test {
-  
+    删除有序数组中的重复项2 main = new 删除有序数组中的重复项2();
+
+    @Test
+    void removeDuplicates1() {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int actual = main.removeDuplicates(nums);
+        assertEquals(5, actual);
+    }
+
+    @Test
+    void removeDuplicates2() {
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        int actual = main.removeDuplicates(nums);
+        assertEquals(7, actual);
+    }
 }
